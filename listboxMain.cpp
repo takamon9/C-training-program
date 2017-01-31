a7cf5eb17fb8c034164c7743506b97e0d3403ff6
Yukihiro Takamoto <organowhatthehell1974@gmail.com>
2017-01-31 14:10:51 +0900

    Create listboxMain.cpp

---
+++ listboxMain.cpp
@@ -0,0 +1,14 @@
+#include "listboxTest.h"
+#include <iostream>
+
+using namespace std;
+using namespace listbox;
+
+int main()
+{
+
+	listboxTest^ test = gcnew listboxTest();
+	test->ShowDialog();
+
+	return 0;
+}
